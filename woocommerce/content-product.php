d67ec21108eb6a9894575d3f85fddbf3ed6bb448
CreativeBrenz <brendon@piracuda.com>
2016-11-15 13:58:13 +0000

    added line to include product excerpt
    this is meant solely for addition of audio files.

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -46,6 +46,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 				do_action( 'woocommerce_after_shop_loop_item' );
 				do_action( 'woocommerce_after_shop_loop_item_title' );
 
+				 woocommerce_template_single_excerpt();
+
 			?>
 		</div>
 	</article>
