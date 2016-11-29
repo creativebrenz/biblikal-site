213bdccf2d7aca2e800079b36155f4f9219cb3b1
CreativeBrenz <brendon@piracuda.com>
2016-11-29 17:43:55 +0000

    Layout Change
    Changed some images and stylesheet rules to accomodate new layout.

--- header.php
+++ header.php
@@ -4,14 +4,14 @@
 
 	<head>
 		<meta charset="utf-8">
-		
+
 		<!-- Force IE to use the latest rendering engine available -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 		<!-- Mobile Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta class="foundation-mq">
-		
+
 		<!-- If Site Icon isn't set in customizer -->
 		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
 			<!-- Icons & Favicons -->
@@ -33,34 +33,34 @@
 		<!-- end analytics -->
 
 	</head>
-	
-	<!-- Uncomment this line if using the Off-Canvas Menu --> 
-		
+
+	<!-- Uncomment this line if using the Off-Canvas Menu -->
+
 	<body <?php body_class(); ?>>
 
 		<div class="off-canvas-wrapper">
-			
+
 			<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-				
-				<section class="row column">
-				
+
+				<section class="expanded row">
+
 				<?php get_template_part( 'parts/content', 'offcanvas' ); ?>
-				
+
 				<div class="off-canvas-content" data-off-canvas-content>
-					
+
 					<header class="header" role="banner">
 						<div id="title-header">
-							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/header.jpg" alt="header">
+							<h1 class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/header-title.png" alt=""></h1>
 							<ul id="social-header" class="menu simple">
-								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt=""></a></li>
-								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt=""></a></li>
-								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.png" alt=""></a></li>
-								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.png" alt=""></a></li>
+								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/xfacebook.png" alt=""></a></li>
+								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/xinstagram.png" alt=""></a></li>
+								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/xtwitter.png" alt=""></a></li>
+								<li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/xyoutube.png" alt=""></a></li>
 							</ul>
 						</div>
-							
-						 <!-- This navs will be applied to the topbar, above all content 
+
+						 <!-- This navs will be applied to the topbar, above all content
 							  To see additional nav styles, visit the /parts directory -->
 						 <?php get_template_part( 'parts/nav', 'offcanvas-topbar-centered' ); ?>
-		 	
+
 					</header> <!-- end .header -->
--- template-bio.php
+++ template-bio.php
@@ -5,13 +5,13 @@ Template Name: Bio
 ?>
 
 <?php get_header(); ?>
-			
+
 	<div id="content" class="bio-page">
-	
+
 		<div id="inner-content" class="row">
-	
-		    <main id="main" class="medium-6 medium-offset-6 columns" role="main">
-				
+
+		    <main id="main" class="medium-7 medium-offset-5 columns" role="main">
+
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
@@ -20,21 +20,21 @@ Template Name: Bio
 						    <?php the_content(); ?>
 						    <?php //wp_link_pages(); ?>
 						</section> <!-- end article section -->
-											
+
 						<footer class="article-footer">
-							
+
 						</footer> <!-- end article footer -->
-											    
+
 						<?php //comments_template(); ?>
-										
+
 					</article> <!-- end article -->
-					
-				<?php endwhile; endif; ?>							
+
+				<?php endwhile; endif; ?>
 
 			</main> <!-- end #main -->
-		    
+
 		</div> <!-- end #inner-content -->
-	
+
 	</div> <!-- end #content -->
 
 <?php get_footer(); ?>
