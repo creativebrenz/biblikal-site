fafdf61dabcb07d10aed31cf875df4018e272d4f
CreativeBrenz <brendon@piracuda.com>
2016-12-07 18:05:44 +0000

    added menu-toggle class
    at the moment it's purely semantic

--- parts/nav-offcanvas-topbar-centered.php
+++ parts/nav-offcanvas-topbar-centered.php
@@ -3,12 +3,12 @@
 
 <div class="top-bar" id="top-bar-menu">
 	<div class="show-for-medium menu-centered">
-		<?php joints_top_nav(); ?>	
+		<?php joints_top_nav(); ?>
 	</div>
 	<div class="top-bar-right float-right show-for-small-only">
 		<ul class="menu">
 			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
+			<li><a class="menu-toggle" data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
 		</ul>
 	</div>
 </div>
